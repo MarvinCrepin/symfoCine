e33bca520272d793d01a9059ceb2242aa7925d13
Sayn <marvincrepin@gmail.com>
2021-10-13 10:26:15 +0200

    Validating Card CSS from W3C

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -26,7 +26,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoDashboard('Panneau central', 'fa fa-home');
+        // yield MenuItem::linkToCrud('Film', 'fas fa-list', Film::class);
     }
 }
