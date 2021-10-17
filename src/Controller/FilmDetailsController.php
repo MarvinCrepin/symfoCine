72fcfe21b5a99b3591fae8d56ed29acc7c3b4d70
Sayn <marvincrepin@gmail.com>
2021-10-17 18:16:13 +0200

    Added comment function V1.3

--- src/Controller/FilmDetailsController.php
+++ src/Controller/FilmDetailsController.php
@@ -3,13 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
-use App\Form\CommentsType;
+use App\Form\CommentType;
+use App\Entity\User;
+use App\Entity\Film;
 use App\Repository\FilmRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route; 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 /**
      * @Route("/film", name="film_")
@@ -36,4 +39,44 @@ class FilmDetailsController extends AbstractController
             
         return $this->render('film_details/index.html.twig', compact('film'));
     }
+    public function __construct(Security $security)
+    {
+       $this->security = $security;
+    }
+    /**
+     * @Route("/details/{slug}/comment", name="app_comment")
+     */
+    public function comment($slug, Request $request): Response
+    {
+        $filmComment = $this->getDoctrine()
+            ->getRepository(Film::class)
+            ->findOneBy(['slug' => $slug]);
+        $user = $this->security->getUser();
+
+        //Création du formulaire pour récupérer le commentaire
+        $commentaire = new Comment();
+
+        $form = $this->createForm(CommentType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $commentaire->setFilm($filmComment)
+                ->setAuthor($user);
+
+            $em = $this->getDoctrine()
+                ->getManager();
+
+            $em->persist($commentaire);
+
+            $em->flush();
+        }
+
+        return $this->render('film_details/comment.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
 }
+
+
