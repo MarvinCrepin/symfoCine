cfcaea48aa1b848efd1c16dac8addb132b741ba3
Sayn <marvincrepin@gmail.com>
2021-10-17 15:13:01 +0200

    Added footer with copyright/linkedin

---
+++ src/Controller/AdvicesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdvicesController extends AbstractController
+{
+    /**
+     * @Route("/advices", name="advices")
+     */
+    public function index(): Response
+    {
+        return $this->render('advices/index.html.twig', [
+            'controller_name' => 'AdvicesController',
+        ]);
+    }
+
+    public function addLike() {
+        
+    }
+}
